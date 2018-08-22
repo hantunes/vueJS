9758d7511411659c99b1fa1fa2f64b3d4e15e244
Hugo <hugo.antunes@wit-software.com>
2018-08-22 17:45:06 +0100

    no message

--- vue.js
+++ vue.js
@@ -1,9 +1,13 @@
 new Vue({
 	el: '#app',
   data: {
-    title: 'Hello World!',
+    title: 'cc!',
     link: 'http://google.pt',
-    finishLink: '<a href="http://google.pt">Google</a>'
+    finishLink: '<a href="http://google.pt">Google</a>',
+    counter:0,
+    x:0,
+    y:0
+
   },
   methods: {
   	changeGreeting: function(event) {
@@ -12,6 +16,19 @@ new Vue({
     sayHello: function(event) {
       this.title="HHH";      
     	return this.title;
+    },
+    increase: function(step,event)
+    {
+      this.counter += step;
+    },
+    updateCoordinates: function(event){
+
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    alertMe: function(event){
+
+     alert("alert");
     }
   }
 });

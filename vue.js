4f4478e6592121a6bd476fcddcf6faf47011adce
Hugo <hugo.antunes@wit-software.com>
2018-08-22 15:50:34 +0100

    cc

--- vue.js
+++ vue.js
@@ -1,11 +1,17 @@
 new Vue({
 	el: '#app',
   data: {
-  	title: 'Hello World!'
+    title: 'Hello World!',
+    link: 'http://google.pt',
+    finishLink: '<a href="http://google.pt">Google</a>'
   },
   methods: {
-  	changeTitle: function(event) {
+  	changeGreeting: function(event) {
     	this.title = event.target.value;
+    },
+    sayHello: function(event) {
+      this.title="HHH";      
+    	return this.title;
     }
   }
 });

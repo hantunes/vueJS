4f89b1648889b19e1e03c0883e9d31f4f5055ac3
Hugo <hugo.antunes@wit-software.com>
2018-08-28 10:50:24 +0100

    no message

--- vue.js
+++ vue.js
@@ -1,11 +1,21 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    cClass:''
   },
   methods: {
-    startEffect: function() {
-    
+    startEffect: function() {    
+        
+      console.log("ENTROU");
+      var vm = this;
+      setInterval  (
+        function()
+        {
+          console.log("I");
+          vm.cClass= vm.cClass == '.highlight' ? '.shrink':'.highlight';
+        }
+        ,2000);
     }
-  }
+
+    }  
 });

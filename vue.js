36000ca75eced852922e450f680d0759ae011f94
Hugo <hugo.antunes@wit-software.com>
2018-08-29 16:43:40 +0100

    no message

--- vue.js
+++ vue.js
@@ -1,6 +1,11 @@
 new Vue({
 	el: '#app',
 	data: {
-		show: true
+    show: true,
+    ingredients:['meat','fruit','cookies'],
+    persons:[
+      {name:'Max',age:27,color:'red'},
+      {name:'Anna',age:'unknown',color:'blue'}
+    ]
 	}
 });

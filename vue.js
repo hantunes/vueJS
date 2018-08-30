1c61ba55a27ef11e613fc11c00880401cbfcd6c1
Hugo <hugo.antunes@wit-software.com>
2018-08-30 11:40:21 +0100

    no message

--- vue.js
+++ vue.js
@@ -1,47 +1,17 @@
 new Vue({
   el: '#exercise',
   data: {
-    mClass: '',
-    timer: '',
-    timerProgress: '',
-    color: 'blue',
-    sizeSmall: false,
-    userClass:'',
-    user2Class:'',
-    useMyClass:false,
-    userInput5:'',
-    mWith:0
-   
-    
-  },
-  methods: {
-    startEffect: function () {
-      var vm = this;
-      this.mClass = 'highlight';
-
-      if (this.timer == '') {
-        this.timer = setInterval(
-          function () {
-            vm.mClass = vm.mClass == 'highlight' ? 'shrink' : 'highlight';
-            vm.sizeSmall = vm.sizeSmall == true ? false: true;
-          }, 2000);
-      }
+    showP:true,
+    array: ['Max', 'Anna', 'Chris', 'Manu'],
+    myObject: {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkiens',
+      books: '3'
     },
-    startProgress : function()
-    {
-      
-      if (this.timerProgress == '') {
-        var vm = this;
-        this.timerProgress = setInterval(
-          function () {
-          console.log("TIMER");
-              if(vm.mWith <100)
-               vm.mWith = vm.mWith +1;
-              else
-              clearInterval(vm.timerProgress);
-
-          }, 100);
-      }
+    testData: {
+      name: 'TESTOBJECT', 
+      id: 10,
+      data: [1.67, 1.33, 0.98, 2.21]
     }
   }
 });

6c48d14097ff765273de33e647c35a89b632fb17
Hugo <hugo.antunes@wit-software.com>
2018-09-07 18:36:43 +0100

    no message

--- vue.js
+++ vue.js
@@ -1,11 +1,106 @@
 new Vue({
 	el: '#app',
 	data: {
-    show: true,
-    ingredients:['meat','fruit','cookies'],
-    persons:[
-      {name:'Max',age:27,color:'red'},
-      {name:'Anna',age:'unknown',color:'blue'}
-    ]
-	}
+    playerHealth: 100,
+    monsterHealth:100,
+    gameIsRunning:false,
+    turns:[]
+  },
+  methods:{
+    startGame: function()
+    {
+      this.gameIsRunning = true;
+      this.monsterHealth = 100;
+      this.playerHealth = 100;
+      this.turns =               [];
+    },
+    attack: function()
+    {  
+     var damage= this.monsterHealth -= this.calculateDamage(3,10);
+      this.turns.unshift({
+        isPlayer:true,
+        text: "Player hits Monster for " + damage
+      });
+      if(this.checkWin())
+      {
+        return;
+      }
+      this.monsterAttacks();
+    },
+    specialAttack: function()
+    {
+      var damage = this.calculateDamage(10,20);
+      this.monsterHealth -= damage;
+
+      this.turns.unshift({
+        isPlayer:true,
+        text: "Player hits Monster for " + damage
+      });
+
+      if(this.checkWin())
+      {
+        return;
+      }
+      
+
+     this.monsterAttacks();
+    },
+    heal: function()
+    {
+      if(this.playerHealth <=90)
+        this.playerHealth += 10;
+      else
+        this.playerHealth = 100;
+
+        this.turns.unshift({
+          isPlayer:true,
+          text: "Player heals 10"
+        });
+
+      this.monsterAttacks();
+    },
+    giveUp: function()
+    {
+      this.gameIsRunning = false;
+    },
+    monsterAttacks:function()
+    {
+      var damage = this.calculateDamage(5,12);
+      this.playerHealth -= damage;
+      this.checkWin();
+
+      this.turns.unshift({
+        isPlayer:false,
+        text: "Monster hits player for " + damage
+      });
+    },
+    calculateDamage : function(min, max)
+    {
+     return  Math.max( Math.floor((Math.random()*max)) + 1,min);
+    },
+    checkWin: function()
+    {
+      if(this.monsterHealth <=0)
+      {
+        if(confirm("you won new game ? "))
+        {
+          this.startGame();
+        }
+        else
+        this.gameIsRunning = false;
+        return true;
+        
+      }else if(this.playerHealth <=0)      
+      {
+        if(confirm("You Lost new game ? "))
+        {
+          this.startGame();
+        }
+        else
+        this.gameIsRunning = false;
+        return true;
+      }
+      return false;
+    }
+  }
 });

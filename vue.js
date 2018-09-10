e1424f615d789224644d9439a98a18ba6ddb450f
Hugo <hugo.antunes@wit-software.com>
2018-09-10 17:56:54 +0100

    no message

--- vue.js
+++ vue.js
@@ -1,71 +1,47 @@
-var data = {
-  title: 'The  Instance',
-  showParagraph: false
-}
 
-Vue.component('hello',{
-  template:'<h1>Hello! CC</h1>'
-});
 
-var vm1 = new Vue({  
-  data: data,
-  methods: {
-    show: function() {
-      this.showParagraph = true;
-      this.updateTitle('The  Instance (Updated)');
-      console.log(this.$refs.myButton);
-      this.$refs.myButton.innerText = 'Test';
-    },
-    updateTitle: function(title) {
-      this.title = title;
-    }
-  },
-  computed: {
-    lowercaseTitle: function() {
-      return this.title.toLowerCase();
-    }
-  },
-  watch: {
-    title: function(value) {
-     // alert('Title changed, new value: ' + value);
-    }
-  }
-});
-
-vm1.$mount('#app1');
-
-vm1.newProp = 'Val';
-console.log(vm1.$data.title);
-
-vm1.$refs.heading.innerText = "something else";
-
-setTimeout ( function(){
- 
-  vm1.title = "Changed by timer";
- // vm1.show();
-}
-  ,3000)
-
-var vm2 = new Vue({
-  el: '#app2',
+new Vue({  
+  el:'#app',
   data: {
-    title: 'The  Instance 2',
-    showParagraph: false
+    title: 'The Instance'
   },
-  methods: {
-    onChange: function() {
-     vm1.title = "Changed";
-      
-    }
-  } 
+  beforeCreate:  function() {
+      console.log("BeforeCreate()");
+    },
   
+    created:  function() {
+      console.log("Create()");
+      
+    },
+    beforeMount:  function() {
+      console.log("beforeMount()");
+      
+    },
+    mounted:  function() {
+      console.log("mounted()");
+      
+    },
+    beforeUpdate:  function() {
+      console.log("beforeUpdate()");
+      
+    },
+    updated:  function() {
+      console.log("updated()");
+      
+    },
+    beforeDestroy:  function() {
+      console.log("beforeDestroy()");
+      
+    },
+    destroyed:  function() {
+      console.log("destroyed()");
+      
+    },
+    methods:{
+
+      destroy:function()
+      {
+        this.$destroy();
+      }
+    }
 });
-
-
-var vm3 = new Vue({
-el:'.hello',
-  template:'<h1>Hello App33</h1>'
-});
-
-//vm3.$mount();
-//document.getElementById('app3').appendChild(vm3.$el);

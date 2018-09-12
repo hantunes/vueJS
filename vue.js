21a12fee05aca552f360e8a5247acc231615d1a0
Hugo <hugo.antunes@wit-software.com>
2018-09-12 15:36:43 +0100

    no message

---
+++ vue.js
@@ -0,0 +1,24 @@
+
+ Vue.component('my-cmp',{
+
+  // data tem de ser um função
+  data: function(){
+    return{
+    status:"Hello"
+    }
+  },
+  template: '<p> Server status: {{status}} (<button @click=ChangeStatus> Change Status</button>)</p>',
+  methods: {
+    ChangeStatus: function()
+    {
+      this.status = 'Normal';
+    }
+  }
+});
+
+
+new Vue({
+	el: '#app'
+ 
+});
+

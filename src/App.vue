f4da6c079c7ad881b22b2f73b52182431505a525
Hugo <hugo.antunes@wit-software.com>
2018-09-17 17:25:25 +0100

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,41 @@
 <template>
     <div class="container">
-        <app-header></app-header>
-        <hr>
         <div class="row">
-            <servers></servers>
-            <app-server-details  @itemClickStatus="itemStatus= $event"></app-server-details>
+            <div class="col-xs-12">
+                <!-- <button @click="selectedComponent= 'appQuote'">Quote</button>
+                <button @click="selectedComponent= 'appAuthor'">Author</button>
+                <button @click="selectedComponent= 'appNew'">New</button> -->
+                
+                <hr>
+                <!-- <p>{{selectedComponent}}</p> -->
+               <app-quote >
+                    <h2 slot="title">{{quoteTitle}}</h2>
+                    <p >A winderfull Quote!</p>
+
+               </app-quote>
+            </div>
         </div>
-        <hr>
-        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
-    import Header from './components/Shared/Header.vue';
-    import Footer from './components/Shared/Footer.vue';
-    import Servers from './components/Server/Servers.vue';
-    import ServerDetails from './components/Server/ServerDetails.vue';
-
-    export default {
-        components: {
-            appHeader: Header,
-            Servers,
-            'app-server-details': ServerDetails,
-            'app-footer': Footer
-        }
-    }
+ import Quote from "./components/Quote.vue";
+// import Author from "./components/Author.vue";
+// import New from "./components/New.vue";
+export default {
+  data: function() {
+    return {
+      quoteTitle: "The Quote1",
+      selectedComponent:'appQuote'
+    };
+  },
+  components: {
+    appQuote: Quote,
+    // appAuthor: Author,
+    // appNew:New
+  }
+};
 </script>
 
 <style>
-
 </style>

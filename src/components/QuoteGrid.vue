76e07b252f51cb0c659913479e5cbed5beb93825
Hugo <hugo.antunes@wit-software.com>
2018-09-18 13:46:24 +0100

    no message

---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="row">
+         <app-quote v-for='(quote, index) in quotes' @click.native="deleteQuote(index)" >{{quotes}}</app-quote>
+    </div>
+</template>
+
+<script>
+import Quote from "./Quote.vue";
+
+export default {
+  props: ["quotes"],
+  components: {
+    appQuote: Quote
+  },
+  methods:{
+      deleteQuote(index)
+      {
+        this.$emit("QuoteDeleted",index);
+      }
+  }
+
+};
+</script>
+
+<style>
+</style>

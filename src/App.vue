a5092f453704c3c8c8c1013877d7475922160d4e
Hugo <hugo.antunes@wit-software.com>
2018-09-24 16:57:55 +0100

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
-        <form>
-            <div class="row">
+        <form v-if="!isSubmitted">
+            <div class="row" >
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
@@ -9,28 +9,18 @@
                     <!-- Mail -->
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
+                       <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
 
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
 
-                    <h1>File a Complaint</h1>
+                    <h1>Form</h1>
                     <hr>
                     <div class="form-group">
-                        <div class="nameDivs">
-                            <label for="FirstName"> Fisrt Name</label>
-                            <input
-                                    type="text"
-                                    id="FirstName"
-                                    class="form-control"
-                                    v-model="userData.firstName">
-                        </div>   
-                        
-                        <div class="nameDivs">
-                            <label for="LastName"> Last Name</label>
-                            <input
-                                    type="text"
-                                    id="LastName"
-                                    class="form-control"
-                                    v-model="userData.lastName">
-                        </div>
+                         <app-input-name v-model="name"></app-input-name>
                     </div>
 
                     <div class="form-group">
@@ -49,29 +39,35 @@
                                 type="password"
                                 id="password"
                                 class="form-control"                                 
-                                  v-model="userData.password"
-                                  >
+                                  v-model="userData.password">
+                    </div>                    
+
+                    <div class="form-group">
+                        <label for="storeData">Store Data</label>                       
+                        <div id="on" @click="switched(true)" :class="{active: userData.storeData}">On</div>
+                        <div id="off" @click="switched(false)" :class="{active: !userData.storeData}">Off</div>
                     </div>
-
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
+                 
+                </div>
+                 <hr>
+                    <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                
+                    <button class="btn btn-primary" @click.prevent="Submited" >Submit! </button>
+                    
+                </div>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted" >
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: {{userData.firstName}} {{userData.lastName}} </p>
+                        <p>Full Name: {{name}} </p>
                         <p>Mail: {{userData.email}}</p>
                         <p>Password:{{userData.password}} </p>
                         <p>Store in Database?: {{userData.storeData}}</p>
@@ -83,24 +79,55 @@
 </template>
 
 <script>
+
+import InputName  from "./InputName.vue";
+
 export default {
   data() {
     return {
       userData: {
-        firstName: "",
-        lastName: "",
+        name: "",       
         email: "",
         password: "",
-        storeData: true,
-      }
+        storeData: false
+      },
+      isSubmitted :false
     };
+  },
+  methods: {
+    switched(isToStoreData) {
+        this.userData.storeData = isToStoreData;
+    },
+    Submited() {
+        this.isSubmitted = true;
+    }
+  },
+  components: {
+    appInputName: InputName
   }
 };
 </script>
 
-<style>
+<style scoped>
 
-.nameDivs{
-    display: inline-block;
+#on,
+#off {
+  width: 40px;
+  height: 20px;
+  background-color: lightgray;
+  padding: 2px;
+  display: inline-block;
+  margin: 10px -2px;
+  box-sizing: content-box;
+  cursor: pointer;
+  text-align: center;
+}
+#on:hover,
+#on.active {
+  background-color: lightgreen;
+}
+#off:hover,
+#off.active {
+  background-color: lightcoral;
 }
 </style>

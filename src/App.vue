c34462b40b6e2fec33ece9b97ead6c0c557734e1
Hugo <hugo.antunes@wit-software.com>
2018-10-03 18:22:06 +0100

    no message

--- src/App.vue
+++ src/App.vue
@@ -2,79 +2,70 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1 class="text-center">The Super Quiz</h1>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <transition name="flip" mode="out-in">
-                    <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
-                </transition>
+                <h1>Http</h1>
+                <div class="form-group">
+                  <label>Username</label>
+                  <input class="form-control" type="text" v-model="user.username">
+                </div>
+                 <div class="form-group">
+                  <label>Mail</label>
+                  <input class="form-control" type="text" v-model="user.email">
+                </div>
+                <button class="btn btn-primary" @click="submit">Submit</button>
+                <hr>
+                <button class="btn btn-primary" @click="fetchData">Get Data</button>
+                <hr>
+               
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="u in users">{{u.username}} - {{u.email}}</li>
+                </ul>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Question from './components/Question.vue';
-    import Answer from './components/Answer.vue';
-
     export default {
-        data() {
-            return {
-                mode: 'app-question'
-            }
-        },
-        methods: {
-          answered(isCorrect) {
-              if (isCorrect) {
-                  this.mode = 'app-answer';
-              } else {
-                  this.mode = 'app-question';
-                  alert('Wrong, try again!');
-              }
-          }
-        },
-        components: {
-            appQuestion: Question,
-            appAnswer: Answer
+      data()
+      {
+        return{
+          user:{
+            username:'',
+            email:''
+          },
+          users:[]
         }
+      },
+      methods:{
+        submit(){
+          //console.log(this.user);
+          // atraves do vue-resources 
+          this.$http.post('https://vuejs-http-25766.firebaseio.com/data.json',this.user)          
+            .then(response=>{
+              console.log(response);
+            },error=>{
+              console.log(error);
+            });
+
+        },
+        fetchData()
+        {
+          this.$http.get('https://vuejs-http-25766.firebaseio.com/data.json')
+          .then(response=>{
+            return response.json();
+            
+          }).then(data=> {
+            const resultArray = [];
+            for (let key in data)
+            {
+              resultArray.push(data[key]);
+            }
+            this.users = resultArray;
+          });
+        }
+      }
     }
 </script>
 
 <style>
-    .flip-enter {
-        /*transform: rotateY(0deg);*/
-    }
-
-    .flip-enter-active {
-        animation: flip-in  0.5s ease-out forwards;
-    }
-
-    .flip-leave {
-        /*transform: rotateY(0deg);*/
-    }
-
-    .flip-leave-active {
-        animation: flip-out 0.5s ease-out forwards;
-    }
-
-    @keyframes flip-out {
-        from {
-            transform: rotateY(0deg);
-        }
-        to {
-            transform: rotateY(90deg);
-        }
-    }
-
-    @keyframes flip-in {
-        from {
-            transform: rotateY(90deg);
-        }
-        to {
-            transform: rotateY(0deg);
-        }
-    }
 </style>

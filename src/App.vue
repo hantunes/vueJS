33e7bee912cb7a86195f1fb58c215e662e912791
Hugo <hugo.antunes@wit-software.com>
2018-10-03 16:48:16 +0100

    no message

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,84 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
-                <!-- Exercise 1) -->
-                <!-- Build a local Filter which reverses the Text it is applied on -->
-                    <p>{{text | reverseText}}</p>
-                <!-- Exercise 2 -->
-                <!-- Build a global Filter which counts the length of a word and it appends it -->
-                <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
-                    <p>{{text | counts-length}}</p>
-                <!-- Exercise 3 -->
-                <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
-                    <p>{{reverseTextComputed}}</p>
-                    <p>{{countsLengthComputed}}</p>
+                <h1>Animations</h1>
+                <hr>
+                <select v-model="alertAnimation" class="form-control" >
+                  <option value="Fade">Fade</option>
+                  <option value="Slide">Slide</option>
+                </select>
+                <br>
+                <br>
+                <button class="btn btn-primary" @click="show= !show"> Show Alert</button>
+                <transition :name="alertAnimation">
+                    <div class="alert alert-info" v-if="show"> This is some Info</div>
+                </transition>
 
-                <!-- Exercise 4 -->
-                <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
-                 <p>{{countsLengthMixin}}</p>
+                  <transition name="slide" type="animation"> <!--o type é para ditar qual dos dos tempos (animations e transition ) o vue deve ter conta  -->
+                    <div class="alert alert-info" v-if="show"> This is some Info</div>
+                </transition>
+                    <!-- definir os nomes das class, neste caso é para usar class´s das libraria animate.css -->
+                 <transition                                      
+                    enter-active-class="animated bounce"                    
+                    leave-active-class="animated shake"                  
+                  >
+                    <div class="alert alert-info" v-if="show"> This is some Info</div>
+                </transition>
+
+
+                  <transition :name="alertAnimation" mode="out-in"> 
+                    <div class="alert alert-info" v-if="show" key="info"> This is some Info</div>
+                    <div class="alert alert-warning" v-else key="warn"> This is some War</div>
+                </transition>
+              <hr>
+
+              <button class="btn btn-primary" @click="load=!load"> Load / Remove Element</button>
+              <br>
+
+              <!-- animações com javascript  -->
+              <transition 
+              @before-enter="beforeEnter"
+              @enter="enter"
+              @after-enter="afterEnter"
+              @enter-cancelled="enterCancelled"
+              
+              @before-leave="beforeLeave"
+              @leave="leave"
+              @after-leave="afterLeave"
+              @leave-cancelled="leaveCancelled"
+              :css="false"
+              >
+                <!-- // css=false para o v.js não procurar por css  -->
+                <div style="width:300px; height:100px; background-color: lightgreen" v-if="load"></div>
+              </transition>
+
+            <hr>
+            <button class="btn btn-primary"
+            @click="selectedComponent == 'app-success-alert' ? selectedComponent = 'app-danger-alert' : selectedComponent = 'app-success-alert'"
+            >Toggle Components</button>
+            <hr>
+            <hr>
+            <transition name="fade" mode="out-in">
+                <component :is="selectedComponent"></component>
+            </transition>
+            
+
+            <hr>
+             Transitions Group 
+             <hr>
+             <button class="btn btn-primary" @click="addItem" >Add Item </button>
+             <hr>
+            <ul class="list-group" >
+
+              <transition-group name="slide" >
+
+                  <li 
+                  class="list-group-item"
+                  v-for="(number,index) in numbers"
+                    @click="removeItem(index)" style="cursor:pointer" :key="number">{{number}}</li>
+
+                </transition-group>
+            </ul>
 
             </div>
         </div>
@@ -25,32 +87,139 @@
 </template>
 
 <script>
-import { countLengthMixin } from "./countLengthMixin.js";
-    export default {
-        mixins:[countLengthMixin],
-        data()
-        {
-            return{
-                text:'Hello there',
-               
-            }
-        },
-        filters:{
-            reverseText(value){
-               return value.split('').reverse().join('');
-            }
-        },
-      computed: {
-        reverseTextComputed() {
-            return this.text.split('').reverse().join('');
-        },
-        countsLengthComputed() {
-            return this.text+ " (" + this.text.length + ")";
-        }
-      }
+import DangerAlert from './Danger.vue';
+import SuccessAlert from './Success.vue';
+export default {
+  data() {
+    return {
+      show: false,
+      load: true,
+      alertAnimation:'Fade',
+      elementWidth: 100,
+      selectedComponent: 'app-success-alert',
+      numbers:[1,2,3,4,5]
+    };
+  },
+  methods:{
+    beforeEnter(el){
+      console.log("beforeEnter");
+      this.elementWidth = 100;
+      el.style.width = this.elementWidth + 'px';
+    },
+    enter(el,done){
+      console.log("enter");
+      let round =1;
+      const interval = setInterval(()=> {
+        el.style.width = (this.elementWidth + round *10) + 'px';
+        round ++;
 
+        if(round > 20 )
+        {
+          clearInterval(interval);
+          done();
+        }
+      },20);
+      
+    },
+    afterEnter(el){
+      console.log("afterEnter");     
+    },
+    enterCancelled(el){
+      console.log("enterCancelled");
+    },
+    beforeLeave(el){
+      console.log("beforeLeave"); 
+      this.elementWidth = 300;     
+      el.style.width = this.elementWidth  + 'px';
+    },
+    leave(el,done){
+      console.log("leave");
+      let round =1;
+      const interval = setInterval(()=> {
+        el.style.width = (this.elementWidth - round *10) + 'px';
+        round ++;
+
+        if(round > 20 )
+        {
+          clearInterval(interval);
+          done();
+        }
+      },20);
+    },
+    afterLeave(el){
+      console.log("afterLeave");
+    },
+    leaveCancelled(el){
+      console.log("leaveCancelled");
+    },
+     addItem()
+    {
+      const pos = Math.floor(Math.random() * this.numbers.length);
+      this.numbers.splice(pos,0, this.numbers.length+ 1);
+    },
+    removeItem(index)
+    {
+      this.numbers.splice(index,1);
     }
+  }, 
+  components:{
+    appDangerAlert: DangerAlert,
+    appSuccessAlert:SuccessAlert
+  }
+};
 </script>
 
 <style>
+/* fade-enter apenas 1 frame  */
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-leave {
+  /* opacity: 1;  é o default */
+}
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
+}
+
+.slide-enter {
+    opacity: 0;
+}
+.slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+}
+.slide-leave {
+    
+}
+.slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 1s;
+    opacity:0 ;
+    position: absolute;
+}
+.slide-move{   
+    transition: transform 1s;   
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
 </style>

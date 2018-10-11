b6219301b331ba8697c71f83de1159fa8cd655ea
Hugo <hugo.antunes@wit-software.com>
2018-10-11 15:52:09 +0100

    no message

---
+++ src/components/user/User.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <button @click="navigateToHome" class="btn btn-primary"> Got to Home</button>
+        <p>Loaded ID: {{id}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    },
+    watch: {
+        '$route'(to,from) {
+            this.id=to.params.id;
+        }
+    },
+    methods: {
+        navigateToHome() {
+            this.$router.push('/');
+        },
+    },
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,17 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 import App from './App.vue'
+import {routes} from  './routes';
 
-Vue.use(VueResource);
+Vue.use(VueRouter);
 
-Vue.http.options.root = "https://vuejs-http-25766.firebaseio.com/";
-
-Vue.http.interceptors.push((request,next)=>{
-  console.log(request);
-  if(request.method =='POST'){
-    request.method ='PUT';
-  }
-  next(response=>{
-    response.json = ()=>{return {message:response.body}}
-  });
+const router = new  VueRouter({
+  routes,
+  mode:'history'
 });
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
